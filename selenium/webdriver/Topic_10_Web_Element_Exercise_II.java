cfc73996d5dddb1a4b338d2b81188c81a95b5836
DTA-Test <dangtuananh94.it@gmail.com>
2023-03-13 03:00:25 +0700

    Topic_10_Web_Element_Exervise_II

--- selenium/webdriver/Topic_10_Web_Element_Exercise_II.java
+++ selenium/webdriver/Topic_10_Web_Element_Exercise_II.java
@@ -31,11 +31,51 @@ public class Topic_10_Web_Element_Exercise_II {
 	}
 	
 	@Test
-	public void Login_01_Empty_Email_Password() { //throws InterruptedException {
-		driver.get("https://automationfc.github.io/basic-form/index.html");
-
+	public void Login_01_Empty_Email_Password() { //throws InterruptedException { 
+		driver.get("http://live.techpanda.org/index.php/");
+		driver.findElement(By.xpath("//div[@class='footer']//a[@title='My Account']")).click();
+		
+		driver.findElement(By.id("email")).sendKeys("");
+		driver.findElement(By.id("pass")).sendKeys("");
+		driver.findElement(By.id("send2")).click();
+		
+		Assert.assertEquals(driver.findElement(By.cssSelector("div#advice-required-entry-email")).getText(), "This is a required field.");
+		Assert.assertEquals(driver.findElement(By.cssSelector("div#advice-required-entry-pass")).getText(), "This is a required field.");
 	}
 
+	@Test
+	public void Login_02_Invalid_Email() { //throws InterruptedException {
+		driver.get("http://live.techpanda.org/index.php/");
+		driver.findElement(By.xpath("//div[@class='footer']//a[@title='My Account']")).click();
+//		driver.findElement(By.cssSelector("div.footer a[title='My Account']")).click();
+		driver.findElement(By.id("email")).sendKeys("3123@fsdgfgf");
+		driver.findElement(By.id("pass")).sendKeys("");
+		driver.findElement(By.id("send2")).click();
+
+		Assert.assertEquals(driver.findElement(By.cssSelector("div#advice-validate-email-email")).getText(), "Please enter a valid email address. For example johndoe@domain.com.");
+	}
+	
+	@Test
+	public void Login_03_Password_Les_Than_6_char() { //throws InterruptedException {
+		driver.get("http://live.techpanda.org/index.php/");
+		driver.findElement(By.xpath("//div[@class='footer']//a[@title='My Account']")).click();
+		driver.findElement(By.id("email")).sendKeys("abc@gmail.com");
+		driver.findElement(By.id("pass")).sendKeys("213");
+		driver.findElement(By.id("send2")).click();
+		
+		Assert.assertEquals(driver.findElement(By.cssSelector("div#advice-validate-password-pass")).getText(), "Please enter 6 or more characters without leading or trailing spaces.");		
+	}
+	 
+	@Test
+	public void Login_04_Incorrect_Email() { //throws InterruptedException {
+		driver.get("http://live.techpanda.org/index.php/");
+		driver.findElement(By.xpath("//div[@class='footer']//a[@title='My Account']")).click();
+		driver.findElement(By.id("email")).sendKeys("abcDTA@gmail.com");
+		driver.findElement(By.id("pass")).sendKeys("123456");
+		driver.findElement(By.id("send2")).click();
+
+		Assert.assertEquals(driver.findElement(By.cssSelector("li.error-msg span")).getText(), "Invalid login or password.");		
+	}
 		
 	private void sleepInSecond(long timeout) {
 		try {
@@ -45,7 +85,7 @@ public class Topic_10_Web_Element_Exercise_II {
 			e.printStackTrace();
 		}
 	}
-	
+
 	@AfterClass
 	public void afterClass() {
 		driver.quit();
